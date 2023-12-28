374d6bebe34e6ca600f346e24a2e57fcbfa3837a
beauchama <alexandre.beauchamp96@gmail.com>
2023-12-28 18:54:43 -0500

    Added Translation and Name

---
+++ src/Atlas.Domain/Translations/Name.cs
@@ -0,0 +1,6 @@
+// Copyright (c) Pulsewave. All rights reserved.
+// The source code is licensed under MIT License.
+
+namespace Atlas.Domain.Translations;
+
+public sealed record Name(string Common, string Official);
---
+++ src/Atlas.Domain/Translations/Translation.cs
@@ -0,0 +1,6 @@
+// Copyright (c) Pulsewave. All rights reserved.
+// The source code is licensed under MIT License.
+
+namespace Atlas.Domain.Translations;
+
+public sealed record Translation(string Code, Name Name);

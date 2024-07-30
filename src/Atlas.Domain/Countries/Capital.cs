f679029371417c910b744619e08ada33fe808fcf
beauchama <alexandre.beauchamp96@gmail.com>
2024-07-29 22:33:45 -0400

    Added Country

---
+++ src/Atlas.Domain/Countries/Capital.cs
@@ -0,0 +1,8 @@
+// Copyright (c) Pulsewave. All rights reserved.
+// The source code is licensed under MIT License.
+
+using Atlas.Domain.Geography;
+
+namespace Atlas.Domain.Countries;
+
+public sealed record Capital(string Name, Coordinate Coordinate);
---
+++ src/Atlas.Domain/Languages/Translation.cs
@@ -0,0 +1,6 @@
+// Copyright (c) Pulsewave. All rights reserved.
+// The source code is licensed under MIT License.
+
+namespace Atlas.Domain.Languages;
+
+public sealed record Translation(string Code, string Name);

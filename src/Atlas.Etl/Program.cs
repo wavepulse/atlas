3edad17ea02ada630978ffbfa32eef86e73860a7
beauchama <beauchama@pulsewave.dev>
2024-08-14 18:06:40 -0400

    Configure loggings and httpclients

--- src/Atlas.Etl/Program.cs
+++ src/Atlas.Etl/Program.cs
@@ -1,10 +1,14 @@
 // Copyright (c) Pulsewave. All rights reserved.
 // The source code is licensed under MIT License.
 
+using Atlas.Etl.Configurations;
 using Microsoft.Extensions.Hosting;
 using System.Diagnostics.CodeAnalysis;
 
 HostApplicationBuilder builder = Host.CreateApplicationBuilder();
+
+builder.ConfigureLoggings();
+builder.ConfigureHttpClients();
 
 await builder.Build().RunAsync().ConfigureAwait(false);
 

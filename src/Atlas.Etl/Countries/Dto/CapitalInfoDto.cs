3c4a03c8ee478e85e20ba9ada96bf08d2618050c
beauchama <beauchama@pulsewave.dev>
2024-08-14 19:45:14 -0400

    Added dto for country

---
+++ src/Atlas.Etl/Countries/Dto/CapitalInfoDto.cs
@@ -0,0 +1,8 @@
+﻿// Copyright (c) Pulsewave. All rights reserved.
+// The source code is licensed under MIT License.
+
+using System.Text.Json.Serialization;
+
+namespace Atlas.Etl.Countries.Dto;
+
+internal sealed record CapitalInfoDto([JsonPropertyName("latlng")] CoordinateDto? Coordinate);
---
+++ src/Atlas.Etl/Countries/Dto/SubRegionDto.cs
@@ -0,0 +1,11 @@
+﻿// Copyright (c) Pulsewave. All rights reserved.
+// The source code is licensed under MIT License.
+
+namespace Atlas.Etl.Countries.Dto;
+
+internal enum SubRegionDto
+{
+    NorthAmerica = 0,
+
+    SouthAmerica = 1
+}

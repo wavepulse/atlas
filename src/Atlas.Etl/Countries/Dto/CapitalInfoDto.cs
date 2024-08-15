6f5dcb46185b108cc2a20fc476471f4ef80938f2
beauchama <beauchama@pulsewave.dev>
2024-08-14 20:35:50 -0400

    Added TranslationDtoJsonConverter

--- src/Atlas.Etl/Countries/Dto/CapitalInfoDto.cs
+++ src/Atlas.Etl/Countries/Dto/CapitalInfoDto.cs
@@ -1,8 +1,12 @@
-﻿// Copyright (c) Pulsewave. All rights reserved.
+// Copyright (c) Pulsewave. All rights reserved.
 // The source code is licensed under MIT License.
 
 using System.Text.Json.Serialization;
 
 namespace Atlas.Etl.Countries.Dto;
 
-internal sealed record CapitalInfoDto([JsonPropertyName("latlng")] CoordinateDto? Coordinate);
+internal sealed record CapitalInfoDto
+{
+    [JsonPropertyName("latlng")]
+    public CoordinateDto? Coordinate { get; init; }
+}

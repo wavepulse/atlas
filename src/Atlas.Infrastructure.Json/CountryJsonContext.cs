b15b9aeda4425f9cf3de2e1d6d6a010f42c29038
beauchama <beauchama@pulsewave.dev>
2024-08-16 15:58:27 -0400

    Added Application layer

--- src/Atlas.Infrastructure.Json/CountryJsonContext.cs
+++ src/Atlas.Infrastructure.Json/CountryJsonContext.cs
@@ -3,7 +3,6 @@
 
 using Atlas.Domain.Countries;
 using Atlas.Infrastructure.Json.Converters;
-using System.Diagnostics.CodeAnalysis;
 using System.Text.Json.Serialization;
 
 namespace Atlas.Infrastructure.Json;

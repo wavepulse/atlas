04917f968b1217056205a1b0611ff705e266621c
beauchama <beauchama@pulsewave.dev>
2024-09-02 22:52:52 -0400

    Added CountryAutocomplete with list expanded

---
+++ src/Atlas.Web.App/Components/CountryAutocomplete.razor.cs
@@ -0,0 +1,10 @@
+// Copyright (c) Pulsewave. All rights reserved.
+// The source code is licensed under MIT License.
+
+using Microsoft.AspNetCore.Components;
+
+namespace Atlas.Web.App.Components;
+
+public sealed partial class CountryAutocomplete
+{
+}

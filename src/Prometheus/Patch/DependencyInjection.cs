ae18dd589ec799467c5852abb261060f081da4c0
beauchama <beauchama@pulsewave.dev>
2024-09-18 22:04:25 -0400

    Remove settings

--- src/Prometheus/Patch/DependencyInjection.cs
+++ src/Prometheus/Patch/DependencyInjection.cs
@@ -1,25 +1,15 @@
 // Copyright (c) Pulsewave. All rights reserved.
 // The source code is licensed under MIT License.
 
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Options;
-using Prometheus.Patch.Settings;
+using System.Diagnostics.CodeAnalysis;
 
 namespace Prometheus.Patch;
 
+[ExcludeFromCodeCoverage]
 internal static class DependencyInjection
 {
     internal static void AddPatchServices(this IHostApplicationBuilder builder)
-    {
-        _ = builder.Configuration.AddJsonFile("patch.json", optional: false, reloadOnChange: false);
-
-        _ = builder.Services.Configure<CountryPatchSettings>(builder.Configuration.GetSection(CountryPatchSettings.Section))
-                            .AddSingleton<IValidateOptions<CountryPatchSettings>, CountryPatchSettings.Validator>()
-                            .AddSingleton(sp => sp.GetRequiredService<IOptions<CountryPatchSettings>>().Value)
-                            .AddOptionsWithValidateOnStart<CountryPatchSettings>();
-
-        _ = builder.Services.AddTransient<ICountryPatch, CountryPatch>();
-    }
+        => _ = builder.Services.AddTransient<ICountryPatch, CountryPatch>();
 }

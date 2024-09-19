b9da5b31eddd203bb1c009ea1791899e584033b2
beauchama <beauchama@pulsewave.dev>
2024-09-18 22:04:44 -0400

    Added CountryPatch

--- src/Prometheus/Patch/CountryPatch.cs
+++ src/Prometheus/Patch/CountryPatch.cs
@@ -1,25 +1,26 @@
-﻿// Copyright (c) Pulsewave. All rights reserved.
+// Copyright (c) Pulsewave. All rights reserved.
 // The source code is licensed under MIT License.
 
 using Prometheus.Countries.Dto;
 using Prometheus.Countries.Json;
-using Prometheus.Patch.Settings;
+using System.Diagnostics.CodeAnalysis;
 using SystemTextJsonPatch;
 using SystemTextJsonPatch.Operations;
 
 namespace Prometheus.Patch;
 
-internal sealed class CountryPatch(CountryPatchSettings settings) : ICountryPatch
+[ExcludeFromCodeCoverage]
+internal sealed class CountryPatch : ICountryPatch
 {
-    private readonly Dictionary<string, Operation<CountryDto>[]> _patches = new(settings.Patches, StringComparer.OrdinalIgnoreCase);
-
     public void ApplyTo(ReadOnlySpan<CountryDto> countries)
     {
+        Dictionary<string, Operation<CountryDto>[]> patches = GetCountryPatches();
+
         JsonPatchDocument patch = new([], CountryDtoJsonContext.Default.Options);
 
         foreach (CountryDto country in countries)
         {
-            if (_patches.TryGetValue(country.Cca2, out Operation<CountryDto>[]? operations))
+            if (patches.TryGetValue(country.Cca2, out Operation<CountryDto>[]? operations))
             {
                 patch.Operations.AddRange(operations);
 
@@ -29,4 +30,17 @@ internal sealed class CountryPatch(CountryPatchSettings settings) : ICountryPatc
             }
         }
     }
+
+    private static Dictionary<string, Operation<CountryDto>[]> GetCountryPatches() => new(StringComparer.OrdinalIgnoreCase)
+    {
+        ["cc"] = [new Operation<CountryDto>("replace", "/coordinate/latitude", from: null, -12.1642)],
+        ["hm"] = [new Operation<CountryDto>("replace", "/coordinate/latitude", from: null, -53.0818)],
+        ["pf"] =
+        [
+            new Operation<CountryDto>("replace", "/coordinate/latitude", from: null, -17.6797),
+            new Operation<CountryDto>("replace", "/coordinate/longitude", from: null, -149.4068),
+        ],
+        ["mf"] = [new Operation<CountryDto>("replace", "/coordinate/longitude", from: null, -63.0501)],
+        ["fj"] = [new Operation<CountryDto>("replace", "/coordinate/latitude", from: null, -17.7134)],
+    };
 }

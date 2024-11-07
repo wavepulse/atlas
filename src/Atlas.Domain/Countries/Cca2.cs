c0744fb42a061862a3f0ee7e7fb9a67d08154e1e
Alex <beauchama@pulsewave.dev>
2024-11-06 20:24:56 -0500

    Remove isolated css

--- src/Atlas.Domain/Countries/Cca2.cs
+++ src/Atlas.Domain/Countries/Cca2.cs
@@ -1,6 +1,8 @@
 // Copyright (c) Pulsewave. All rights reserved.
 // The source code is licensed under MIT License.
 
+using System.Diagnostics.CodeAnalysis;
+
 namespace Atlas.Domain.Countries;
 
 public readonly record struct Cca2(string Value)
@@ -9,6 +11,7 @@ public readonly record struct Cca2(string Value)
 
     public bool Equals(Cca2 other) => Value.Equals(other.Value, StringComparison.OrdinalIgnoreCase);
 
+    [ExcludeFromCodeCoverage]
     public override int GetHashCode() => Value.GetHashCode(StringComparison.OrdinalIgnoreCase);
 
     public override string ToString() => Value;

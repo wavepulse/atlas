e891548e406f36744ef4ffb16e9615c357d5cce5
beauchama <beauchama@pulsewave.dev>
2024-11-08 22:53:57 -0500

    Improve FlagGuesses by removing responsability which should not have

--- src/Atlas.Web.App/Games/Flags/Components/FlagGuesses.razor.cs
+++ src/Atlas.Web.App/Games/Flags/Components/FlagGuesses.razor.cs
@@ -3,47 +3,25 @@
 
 using Atlas.Application.Countries.Responses;
 using Atlas.Web.App.Components;
-using Atlas.Web.App.Stores.Games;
-using Fluxor;
+using Microsoft.AspNetCore.Components;
 using System.Globalization;
 
 namespace Atlas.Web.App.Games.Flags.Components;
 
-public sealed partial class FlagGuesses(IActionSubscriber subscriber, IDispatcher dispatcher) : IDisposable
+public sealed partial class FlagGuesses
 {
-    private const int MaxGuesses = 6;
-
-    private readonly List<GuessedCountryResponse> _guesses = new(MaxGuesses);
     private readonly NumberFormatInfo _numberFormat = new()
     {
         NumberGroupSeparator = " "
     };
 
-    private bool _hasWonGame;
+    [Parameter]
+    [EditorRequired]
+    public required IReadOnlyCollection<GuessedCountryResponse> Guesses { get; init; }
 
-    public void Dispose() => subscriber.UnsubscribeFromAllActions(this);
-
-    protected override void OnInitialized()
-    {
-        subscriber.SubscribeToAction<GameActions.GuessResult>(this, action =>
-        {
-            _hasWonGame = action.Country.Success;
-            _guesses.Add(action.Country);
-
-            if (_guesses.Count == MaxGuesses && !_hasWonGame)
-                dispatcher.Dispatch(new GameActions.GameOver());
-
-            StateHasChanged();
-        });
-
-        subscriber.SubscribeToAction<GameActions.Restart>(this, _ =>
-        {
-            _guesses.Clear();
-            _hasWonGame = false;
-
-            StateHasChanged();
-        });
-    }
+    [Parameter]
+    [EditorRequired]
+    public required int MaxAttempts { get; init; }
 
     private static string HasSuccess(bool success) => success ? "success" : "wrong";
 
@@ -51,5 +29,5 @@ public sealed partial class FlagGuesses(IActionSubscriber subscriber, IDispatche
 
     private static string IsSameContinent(bool isSameContinent) => isSameContinent ? $"success {Icons.Check}" : $"wrong {Icons.Times}";
 
-    private string HasWonGame() => _hasWonGame ? "has-won" : string.Empty;
+    private string HasWonGame() => Guesses.Any(c => c.Success) ? "has-won" : string.Empty;
 }

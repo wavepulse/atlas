6c7d51c3f87b16a31fc401f2eb7edab1ba306efb
beauchama <beauchama@pulsewave.dev>
2024-11-16 14:02:34 -0500

    Added translations for default pages

---
+++ src/Atlas.Web.App/Resources.cs
@@ -0,0 +1,23 @@
+// Copyright (c) Pulsewave. All rights reserved.
+// The source code is licensed under MIT License.
+
+namespace Atlas.Web.App;
+
+internal sealed class Resources
+{
+    private Resources()
+    {
+    }
+
+    public const string DailyFlag = nameof(DailyFlag);
+
+    public const string Games = nameof(Games);
+
+    public const string GoBack = nameof(GoBack);
+
+    public const string NotFound = nameof(NotFound);
+
+    public const string NotFoundMessage = nameof(NotFoundMessage);
+
+    public const string RandomFlag = nameof(RandomFlag);
+}

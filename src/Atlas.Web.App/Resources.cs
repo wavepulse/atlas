e514630af6368eece5e79632ecc82627dab0b53e
beauchama <beauchama@pulsewave.dev>
2024-11-16 15:12:06 -0500

    Added translations

--- src/Atlas.Web.App/Resources.cs
+++ src/Atlas.Web.App/Resources.cs
@@ -9,15 +9,39 @@ internal sealed class Resources
     {
     }
 
+    public const string Changelog = nameof(Changelog);
+
+    public const string Congratulations = nameof(Congratulations);
+
+    public const string Continent = nameof(Continent);
+
     public const string DailyFlag = nameof(DailyFlag);
+
+    public const string Direction = nameof(Direction);
+
+    public const string FailedDisplayCountry = nameof(FailedDisplayCountry);
 
     public const string Games = nameof(Games);
 
     public const string GoBack = nameof(GoBack);
 
+    public const string Guess = nameof(Guess);
+
+    public const string LookupPlaceholder = nameof(LookupPlaceholder);
+
     public const string NotFound = nameof(NotFound);
 
     public const string NotFoundMessage = nameof(NotFoundMessage);
 
+    public const string Project = nameof(Project);
+
     public const string RandomFlag = nameof(RandomFlag);
+
+    public const string ReportBug = nameof(ReportBug);
+
+    public const string Restart = nameof(Restart);
+
+    public const string SeeOnMap = nameof(SeeOnMap);
+
+    public const string Settings = nameof(Settings);
 }

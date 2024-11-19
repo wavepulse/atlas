51b04cea2416eacd668393b3e260f49523a79bc0
beauchama <beauchama@pulsewave.dev>
2024-11-18 21:02:58 -0500

    Added GetCountry query

---
+++ src/Atlas.Application/Countries/Queries/GetCountry.cs
@@ -0,0 +1,25 @@
+// Copyright (c) Pulsewave. All rights reserved.
+// The source code is licensed under MIT License.
+
+using Atlas.Application.Countries.Repositories;
+using Atlas.Application.Countries.Responses;
+using Atlas.Application.Countries.Responses.Mappers;
+using Atlas.Domain.Countries;
+using Mediator;
+
+namespace Atlas.Application.Countries.Queries;
+
+public static class GetCountry
+{
+    public sealed record Query(string Cca2) : IQuery<RandomizedCountryResponse>;
+
+    internal sealed class Handler(ICountryRepository repository) : IQueryHandler<Query, RandomizedCountryResponse>
+    {
+        public async ValueTask<RandomizedCountryResponse> Handle(Query query, CancellationToken cancellationToken)
+        {
+            Country? country = await repository.GetAsync(new Cca2(query.Cca2), cancellationToken).ConfigureAwait(false);
+
+            return country!.ToResponse();
+        }
+    }
+}
---
+++ tests/Atlas.Application.Tests/Countries/Queries/GetCountryTests.cs
@@ -0,0 +1,63 @@
+// Copyright (c) Pulsewave. All rights reserved.
+// The source code is licensed under MIT License.
+
+using Atlas.Application.Countries.Repositories;
+using Atlas.Application.Countries.Responses;
+using Atlas.Domain.Countries;
+using Atlas.Domain.Geography;
+using Atlas.Domain.Languages;
+using Atlas.Domain.Resources;
+
+namespace Atlas.Application.Countries.Queries;
+
+public sealed class GetCountryTests
+{
+    private readonly Country _country = CreateCanada();
+
+    private readonly ICountryRepository _countryRepository = Substitute.For<ICountryRepository>();
+
+    private readonly GetCountry.Query _query;
+    private readonly GetCountry.Handler _handler;
+
+    public GetCountryTests()
+    {
+        _countryRepository.GetAsync(_country.Cca2, CancellationToken.None).Returns(_country);
+
+        _query = new GetCountry.Query(_country.Cca2);
+        _handler = new GetCountry.Handler(_countryRepository);
+    }
+
+    [Fact]
+    public async Task HandleShouldGetTheCountry()
+    {
+        await _handler.Handle(_query, CancellationToken.None);
+
+        await _countryRepository.Received(1).GetAsync(_country.Cca2, CancellationToken.None);
+    }
+
+    [Fact]
+    public async Task HandleShouldReturnTheCountry()
+    {
+        RandomizedCountryResponse country = await _handler.Handle(_query, CancellationToken.None);
+
+        country.Cca2.Should().Be(_country.Cca2);
+    }
+
+    private static Country CreateCanada() => new()
+    {
+        Cca2 = new Cca2("CA"),
+        Area = new Area(9984670),
+        Borders = ["USA"],
+        Capitals = [new Capital("Ottawa", new Coordinate(42, 42))],
+        Continent = Continent.NorthAmerica,
+        Coordinate = new Coordinate(60, 95),
+        Population = 38008005,
+        Translations = [new Translation(Language.English, "Canada")],
+        IsExcluded = false,
+        Resources = new CountryResources()
+        {
+            Flag = new Image(new Uri("https://www.countryflags.io/ca/flat/64.png"), "image/png"),
+            Map = new Uri("https://www.google.com/maps/place/Canada")
+        }
+    };
+}

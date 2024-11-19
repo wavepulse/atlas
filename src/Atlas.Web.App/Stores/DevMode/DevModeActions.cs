bd02bea35d2449e5a070500357ef3422bc46903e
beauchama <beauchama@pulsewave.dev>
2024-11-18 21:11:18 -0500

    Added DevMode actions/effect

---
+++ src/Atlas.Web.App/Stores/DevMode/DevModeActions.cs
@@ -0,0 +1,13 @@
+// Copyright (c) Pulsewave. All rights reserved.
+// The source code is licensed under MIT License.
+
+using Atlas.Application.Countries.Responses;
+
+namespace Atlas.Web.App.Stores.DevMode;
+
+public static class DevModeActions
+{
+    public sealed record GetCountry(string Cca2);
+
+    public sealed record GetCountryResult(RandomizedCountryResponse Country);
+}
---
+++ tests/Atlas.Web.App.Tests/Stores/DevMode/DevModeEffectTests.cs
@@ -0,0 +1,45 @@
+// Copyright (c) Pulsewave. All rights reserved.
+// The source code is licensed under MIT License.
+
+using Atlas.Application.Countries.Queries;
+using Atlas.Application.Countries.Responses;
+using Fluxor;
+using Mediator;
+
+namespace Atlas.Web.App.Stores.DevMode;
+
+public sealed class DevModeEffectTests
+{
+    private readonly IDispatcher _dispatcher = Substitute.For<IDispatcher>();
+    private readonly ISender _sender = Substitute.For<ISender>();
+
+    private readonly DevModeEffect _effect;
+
+    public DevModeEffectTests()
+    {
+        _effect = new DevModeEffect(_sender);
+    }
+
+    [Fact]
+    public async Task HandleGetCountryAsyncShouldSendGetCountryQuery()
+    {
+        DevModeActions.GetCountry action = new("CA");
+
+        await _effect.HandleGetCountryAsync(action, _dispatcher);
+
+        await _sender.Received(1).Send(Arg.Is<GetCountry.Query>(q => q.Cca2 == action.Cca2), CancellationToken.None);
+    }
+
+    [Fact]
+    public async Task HandleGetCountryAsyncShouldDispatchGetCountryResult()
+    {
+        RandomizedCountryResponse country = new("CA", "Canada", new ImageResponse(new Uri("https://image.com"), "image/png"), new Uri("https://map.com"));
+        _sender.Send(Arg.Any<GetCountry.Query>(), CancellationToken.None).Returns(country);
+
+        DevModeActions.GetCountry action = new("CA");
+
+        await _effect.HandleGetCountryAsync(action, _dispatcher);
+
+        _dispatcher.Received(1).Dispatch(Arg.Is<DevModeActions.GetCountryResult>(a => a.Country == country));
+    }
+}

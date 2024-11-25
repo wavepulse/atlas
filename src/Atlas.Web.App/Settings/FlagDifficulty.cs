4f3f3a339d01d1cc370a0ffcdf531baf47744d68
beauchama <beauchama@pulsewave.dev>
2024-11-24 21:43:41 -0500

    [WIP] Difficulty

---
+++ src/Atlas.Web.App/Settings/FlagDifficulty.cs
@@ -0,0 +1,13 @@
+// Copyright (c) Pulsewave. All rights reserved.
+// The source code is licensed under MIT License.
+
+namespace Atlas.Web.App.Settings;
+
+public sealed record FlagDifficulty
+{
+    public Difficulty? All { get; init; }
+
+    public Difficulty Randomized { get; init; }
+
+    public Difficulty Daily { get; init; }
+}
